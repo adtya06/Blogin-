a574a92e0ba7e804bba0a041185a570027e12910
Aaditya Singh <singh.aaditya0507@gmail.com>
2025-12-19 01:00:21 +0530

    Fixed the ATLAS issue

--- backend/database/db.js
+++ backend/database/db.js
@@ -6,17 +6,18 @@ console.log("ENV CHECK:", {
 });
 
 function connectDB() {
-    mongoose.connection.once("open", () => {
+  mongoose.connection.once("open", () => {
     console.log("✅ Connected to MongoDB Atlas");
-    });
+  });
 
-    mongoose.connect(process.env.DB_CONNECT)
-        .then(() => {
-            console.log('Connected to MongoDB');
-        })
-        .catch(err => {
-            console.error('MongoDB connection error:', err);
-        });
+  mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => {
+      console.log("Connected to MongoDB");
+    })
+    .catch((err) => {
+      console.error("MongoDB connection error:", err);
+    });
 }
 
 module.exports = connectDB;
